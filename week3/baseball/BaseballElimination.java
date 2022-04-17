93ceae8d05ac386b6ec359d4dd78154206dd0f1a
Ivan Hu <ivanhu2246@gmail.com>
2022-04-17 16:32:00 +0800

    week3 initial commit

---
+++ week3/baseball/BaseballElimination.java
@@ -0,0 +1,64 @@
+/* *****************************************************************************
+ *  Name: Ivan Hu
+ *  Date: 17/04/2022
+ *  Description: Algos 2 Baseball Elimination assignment
+ **************************************************************************** */
+
+public class BaseballElimination {
+    // create a baseball division from given filename in format specified below
+    public BaseballElimination(String filename) {
+        // TODO
+    }
+
+    // number of teams
+    public int numberOfTeams() {
+        // TODO
+        return -1;
+    }
+
+    // all teams
+    public Iterable<String> teams() {
+        // TODO
+        return null;
+    }
+
+    // number of wins for given team
+    public int wins(String team) {
+        // TODO
+        return -1;
+    }
+
+    // number of losses for given team
+    public int losses(String team) {
+        // TODO
+        return -1;
+    }
+
+    // number of remaining games for given team
+    public int remaining(String team) {
+        // TODO
+        return -1;
+    }
+
+    // number of remaining games between team1 and team2
+    public int against(String team1, String team2) {
+        // TODO
+        return -1;
+    }
+
+    // is given team eliminated?
+    public boolean isEliminated(String team) {
+        // TODO
+        return false;
+    }
+
+    // subset R of teams that eliminates given team; null if not eliminated
+    public Iterable<String> certificateOfElimination(String team) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
