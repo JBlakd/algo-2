e544ca228ef5e2fb8b0c524bd32fcf042e87d691
Ivan Hu <ivanhu2246@gmail.com>
2022-04-20 14:50:27 +0800

    week4 initial commit

---
+++ week4/boggle/BoggleSolver.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name: Ivan Hu
+ *  Date: 20/04/2022
+ *  Description: Algos 2 Assignment boggle
+ **************************************************************************** */
+
+public class BoggleSolver {
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary) {
+        
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        // TODO
+        return null;
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word) {
+        // TODO
+        return -1;
+    }
+}
